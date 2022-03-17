332cdb809db98fe60cee216692dedb67749e052e
Jacob Clark <jacob.jh.clark@gmail.com>
2022-03-17 13:53:02 +0000

    Progress

---
+++ js-jest/src/inventory.js
@@ -0,0 +1,8 @@
+const inventory = {
+    AgedBrie: 'Aged Brie',
+    BackstagePass: 'Backstage passes to a TAFKAL80ETC concert',
+    Sulfuras: 'Sulfuras, Hand of Ragnaros',
+    Normal: 'Normal'
+}
+
+module.exports = inventory;
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,85 @@
 const {Shop, Item} = require("../src/gilded_rose");
+const inventory = require('../src/inventory');
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+describe('End of day', () => {
+  it('should decerement sellIn and quality values for a normal item', () => {
+    const normalItem = new Item({
+      name: inventory.Normal,
+      sellIn: 3, 
+      quality: 5
+    });
+
+    const shop = new Shop([normalItem]);
+
+    shop.updateQuality();
+
+    expect(normalItem.sellIn).toBe(2);
+    expect(normalItem.quality).toBe(4);
+  });
+
+  it('should decrement sellIn and increment quality for aged brie', () => {
+    const agedBrie = new Item({
+      name: inventory.AgedBrie, 
+      sellIn: 3, 
+      quality:5
+    });
+
+    const shop = new Shop([agedBrie]);
+
+    shop.updateQuality();
+
+    expect(agedBrie.sellIn).toBe(2);
+    expect(agedBrie.quality).toBe(6);
+  });
+
+
+
+  describe('Backstage Pass', () => {
+    it('sellIn day less than 5 should increase quality by 3', () => {
+      const backstagePass = new Item({
+        name: inventory.BackstagePass, 
+        sellIn: 3, 
+        quality: 5
+      });
+  
+      const shop = new Shop([backstagePass]);
+  
+      shop.updateQuality();
+  
+      expect(backstagePass.sellIn).toBe(2)
+      expect(backstagePass.quality).toBe(8)
+    });
+
+    it('sellIn day less than 10 should increase quality by 2', () => {
+      const backstagePass = new Item({
+        name: inventory.BackstagePass,
+        sellIn: 9, 
+        quality: 5
+      });
+  
+      const shop = new Shop([backstagePass]);
+  
+      shop.updateQuality();
+  
+      expect(backstagePass.sellIn).toBe(8)
+      expect(backstagePass.quality).toBe(7)
+    });
+
+    it('sellIn day less than 0 should set quality to 0', () => {
+      const backstagePass = new Item({
+        name: inventory.BackstagePass,
+        sellIn: 0, 
+        quality: 5
+      });
+  
+      const shop = new Shop([backstagePass]);
+  
+      shop.updateQuality();
+  
+      expect(backstagePass.sellIn).toBe(-1)
+      expect(backstagePass.quality).toBe(0)
+    });
   });
 });
+
+
